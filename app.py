0ba0e5ef6dac2da37ef605509b386e76615f7439
BigCowPat <18152@wc.school.nz>
2022-03-15 11:10:03 +1300

    Update app with new web routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,11 +8,11 @@ def render_homepage():
     return render_template("home.html")
 
 @app.route('/menu')
-def render_homepage():
+def render_menu():
     return render_template("menu.html")
 
 @app.route('/contact')
-def render_homepage():
+def render_contact():
     return render_template("contact.html")
 
 app.run(host="0.0.0.0")
